77f907209fef90159362c654451734b1d8d3b178
miraKSE <153728620+miraKSE@users.noreply.github.com>
2024-11-04 10:18:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 #пишу комментарий для гитхаб.
+#Ответный комментарий от гитхаб
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
